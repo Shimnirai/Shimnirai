b7fc1ca753c2e72e0f16340fc07b9371524bb9a2
Murali Krishnan <murali@hopetutors.com>
2024-02-20 10:46:26 +0530

    Bug Fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { PipeExComponent } from './pipe-ex/pipe-ex.component';
+import { CustomerComponent } from './customer/customer.component';
+import { TopLayerComponent } from './ContentProjection/top-layer/top-layer.component';
+import { ComponentInteractComponent } from './comp-communicate/component-interact/component-interact.component';
+import { ParentComponent } from './comp-communicate/parent/parent.component';
+import { SampleComponent } from './CCommunicate/sample/sample.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'Pipes', component: PipeExComponent },
+    { path: 'Customer', component: CustomerComponent },
+    { path: 'ContentProjection', component: TopLayerComponent },
+    { path: 'ComponentCommunication', component: ComponentInteractComponent },
+    { path: 'ParentToChild', component: ParentComponent},
+    { path: 'ParentToChild', component: SampleComponent },
 
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
